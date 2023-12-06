ab23fc7644251cb22b0cf7cc7bee6deac0b89943
Sully Tania <sullytania@icloud.com>
2023-12-06 21:59:55 +0700

    tambah layout untuk page login dan register

---
+++ resources/views/web/layouts/auth.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>SIPSnack | @yield('title', 'Home')</title>
+
+  <!-- Bootstrap -->
+  @section('css')
+    @vite(['resources/sass/app.scss','resources/js/app.js',])
+  @show
+    </head>
+    <body class="bg-white">
+     
+    <nav class="navbar navbar-expand-lg navbar-light bg-white mt-3 mb-5">
+        <div class="container">
+          <a class="navbar-brand mx-auto" href="#">SIPSnack</a>
+        </div>
+      </nav>
+    </div>
+
+    @yield('content')
+
+@yield('script')
+</body>
+</html>
