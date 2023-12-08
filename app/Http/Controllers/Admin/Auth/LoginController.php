0134945b3a1f2645fab88bc63a9b760bd7451d0d
Sully Tania <sullytania@icloud.com>
2023-12-09 01:29:16 +0700

    memperbaiki login controller admin menggunakan guard

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -5,35 +5,40 @@ namespace App\Http\Controllers\Admin\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
+    use AuthenticatesUsers;
+
+    public function __construct()
+    {
+        $this->middleware('guest:admin_web')->except('logout');
+    }
+
     public function showLoginForm(){
         return view('admin.auth.login');
     }
  
-    public function login(Request $request)
-    {
-        $this->validate($request, [
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
- 
-        if(auth()->guard('admin')->attempt(['email' => $request->input('email'),  'password' => $request->input('password')])){
-            $user = auth()->guard('admin')->user();
-            if($user->is_admin == 1){
-                return redirect()->route('adminDashboard')->with('success','You are Logged in sucessfully.');
-            }
-        }else {
-            return back()->with('error','Whoops! invalid email and password.');
-        }
-    }
- 
     public function logout(Request $request)
     {
-        auth()->guard('admin')->logout();
-        Session::flush();
-        Session::put('success', 'You are logout sucessfully');
-        return redirect(route('adminLogin'));
+        $this->guard()->logout();
+
+        $request->session()->flush();
+
+        $request->session()->regenerate();
+
+        return redirect(route('admin.login'));
+    }
+
+    protected function attemptLogin(Request $request)
+    {
+        $cred = $this->credentials($request);
+        return $this->guard()->attempt($cred);
+    }
+
+    protected function guard()
+    {
+        return auth()->guard('admin_web');
     }
 }
